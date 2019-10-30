d33c9106e8f0fdd0e42cb95f539c39cb0b3d1d53
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2019-10-30 15:03:25 +0300

    Improve next configuration (including better SEO and CSS & assets optimization)

---
+++ web/src/assets/configs/seo.ts
@@ -0,0 +1,29 @@
+import { NextSeoProps } from 'next-seo/lib'
+
+export default {
+  title: 'Official Website -- 3F | Finfine Furniture Factory plc',
+  description: 'Official Website for 3F (Finfine Furniture Factory plc).',
+  additionalMetaTags: [
+    { name: 'author', content: 'kelal tech plc' },
+    { name: 'copyright', content: '2019 © Finfine Furniture Factory plc' },
+    {
+      name: 'keywords',
+      content: '3f,finfine furniture factory plc,furniture company in ethiopia'
+    },
+    { name: 'robots', content: 'index,follow' },
+    { name: 'theme-color', content: '#bc2239' }
+  ],
+
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://ethio3f.com/',
+    site_name: '3F | Finfine Furniture Factory plc'
+  },
+
+  twitter: {
+    handle: '@ethio3f',
+    site: '@ethio3f',
+    cardType: 'summary_large_image'
+  }
+} as NextSeoProps
--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import App from 'next/app'
-import Head from 'next/head'
+import App, { Container } from 'next/app'
+import { DefaultSeo } from 'next-seo/lib'
 
 import '../assets/styles/_app.scss'
+import SEO from '../assets/configs/seo'
 import Layout from '../shared/components/layout/layout'
 
 // AMP support
@@ -13,56 +14,13 @@ class MyApp extends App {
     const { Component, pageProps } = this.props
 
     return (
-      <>
-        <Head>
-          <meta name="theme-color" content="#bc2239" />
-          <meta name="author" content="kelal tech plc" />
-          <meta
-            name="copyright"
-            content="2019 © Finfine Furniture Factory plcc"
-          />
-          <meta
-            name="description"
-            content="Official Website for 3F (Finfine Furniture Factory plc)."
-          />
-          <meta
-            name="keywords"
-            content="3f,finfine furniture factory plc,furniture company in ethiopia"
-          />
-          <meta name="robots" content="index,follow" />
-
-          <meta property="og:url" content="https://ethio3f.com/" />
-          <meta
-            property="og:title"
-            content="3F | Finfine Furniture Factory plc"
-          />
-          <meta
-            property="og:description"
-            content="Official Website for 3F (Finfine Furniture Factory plc)."
-          />
-          <meta property="og:type" content="website" />
-          <meta
-            property="og:site_name"
-            content="3F | Finfine Furniture Factory plc"
-          />
-          <meta property="og:image" content="/promo.jpg" />
-          <meta property="og:locale" content="en_US" />
-
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@ethio3f" />
-          <meta name="twitter:creator" content="@ethio3f" />
-          <meta name="twitter:image" content="/promo.png" />
-
-          <link rel="shortcut icon" href="/favicon.ico" />
-          <link rel="manifest" href="/manifest.json" />
-
-          <title>Official Website -- 3F | Finfine Furniture Factory plc</title>
-        </Head>
+      <Container>
+        <DefaultSeo {...SEO} />
 
         <Layout>
           <Component {...pageProps} />
         </Layout>
-      </>
+      </Container>
     )
   }
 }
