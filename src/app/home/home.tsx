e6f8d3163a73485f445978da3ca3de49891e9d03
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2019-11-10 17:09:54 +0300

    Improve some styles

--- src/app/home/home.tsx
+++ src/app/home/home.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Page from '../../shared/components/page/page'
 
 function Home() {
-  return <Page>Home</Page>
+  return <Page space={false}>Hello, homepage</Page>
 }
 
 export default Home
