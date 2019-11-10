ab6224fefbc8467ce0cc59c6dc2d0b822d9295da
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2019-11-10 16:45:07 +0300

    Navigation for narrow screens

--- src/shared/components/header/header.tsx
+++ src/shared/components/header/header.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import { Content, FlexSpacer } from 'gerami'
+import React, { useState } from 'react'
+import { Button, Content, FlexSpacer, MenuDrop, MenuItem } from 'gerami'
+import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 import './header.scss'
 import LiteImage from '../lite-image/lite-image'
-import Link from 'next/link'
 
 function Header() {
+  const [isNavMenuOpen, setIsNavMenuOpen] = useState(false)
+
   return (
     <div className="header">
       <header className="header-in-1">
@@ -45,6 +49,36 @@ function Header() {
                 <a>News</a>
               </Link>
             </nav>
+          </div>
+          <div className="header-mini-nav-container">
+            <Button
+              className="header-mini-nav-menu"
+              title="Menu"
+              onClick={() => setIsNavMenuOpen(true)}
+            >
+              <FontAwesomeIcon icon={faBars} />
+            </Button>
+            <MenuDrop
+              open={isNavMenuOpen}
+              onClose={() => setIsNavMenuOpen(false)}
+              align="right"
+            >
+              <Link href="/">
+                <MenuItem>Home</MenuItem>
+              </Link>
+              <Link href="/about">
+                <MenuItem>About</MenuItem>
+              </Link>
+              <Link href="/products">
+                <MenuItem>Products</MenuItem>
+              </Link>
+              <Link href="/locations">
+                <MenuItem>Locations</MenuItem>
+              </Link>
+              <Link href="/news">
+                <MenuItem>News</MenuItem>
+              </Link>
+            </MenuDrop>
           </div>
         </Content>
       </header>
