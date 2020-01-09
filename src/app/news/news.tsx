d869bbc57d9b87110fcf245fb87377952a740fa1
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2020-01-09 19:35:55 +0300

    Changes the listings in news page from gerami's Yoga to react-masonry-component

--- src/app/news/news.tsx
+++ src/app/news/news.tsx
@@ -1,6 +1,8 @@
 import React, { useMemo } from 'react'
-import { Block, Content, Yoga } from 'gerami'
+import { Block, Content } from 'gerami'
+import Masonry from 'react-masonry-component'
 
+import './news.scss'
 import Page from '../../shared/components/page/page'
 import { newsMetas } from '../../../data/news-metas'
 import NewsCard from '../../shared/components/news-card/news-card'
@@ -9,6 +11,16 @@ import LiteParallax from '../../shared/components/lite-parallax/lite-parallax'
 function News() {
   const news = useMemo(
     () => newsMetas.filter(newsMeta => newsMeta.active !== false),
+    [newsMetas]
+  )
+
+  const newsCards = useMemo(
+    () =>
+      news.map((newsMeta, i) => (
+        <div className="news-card-sizer padding-bottom-big" key={i}>
+          <NewsCard newsMeta={newsMeta} />
+        </div>
+      )),
     [newsMetas]
   )
 
@@ -30,22 +42,7 @@ function News() {
 
       <Content size="3XL" transparent className="margin-top-big">
         <Block first last>
-          <Yoga maxCol={2}>
-            <div className="top">
-              {news.slice(0, news.length / 2 + 1).map((newsMeta, i) => (
-                <div className="padding-bottom-big" key={i}>
-                  <NewsCard newsMeta={newsMeta} />
-                </div>
-              ))}
-            </div>
-            <div className="top">
-              {news.slice(news.length / 2 + 1).map((newsMeta, i) => (
-                <div className="padding-bottom-big" key={i}>
-                  <NewsCard newsMeta={newsMeta} />
-                </div>
-              ))}
-            </div>
-          </Yoga>
+          <Masonry>{newsCards}</Masonry>
         </Block>
       </Content>
     </Page>
