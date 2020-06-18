f6a174fdf9d72ed8ff5ae0f1a22ba9bf54801004
dagem <dagixmeko@gmail.com>
2020-06-18 17:10:39 +0300

    product detail opens empty arrays

--- src/app/product-detail/product-detail.tsx
+++ src/app/product-detail/product-detail.tsx
@@ -49,7 +49,7 @@ function ProductDetail({ product }: ProductDetailProps) {
           product.productCatagoryType
         )} by (3F) Finfine Furniture Factory: ${product}`}
       />
-      <LiteParallax
+    <LiteParallax
         src={`${
           product.productCatagoryType === 'sofa_set'
             ? product.productTypes[0].eachProduct[1].images[0].formats.large.url
@@ -153,17 +153,25 @@ function ProductDetail({ product }: ProductDetailProps) {
           </Block> */}
           <div className="fg-blackish">
             <Block first last>
-              {product.productTypes.map((productType, key) => (
+              {product.productTypes.map((productType, key) => !productType ? (
+                <>  
+                 Product Type Not Available
+                </>
+              ) : (
                 <div className={`${styles['sub-product-type']}`} key={key}>
                   <h1>{productType.name}</h1>
                   <p>
                     <Markdown>{productType.descriptions}</Markdown>
                   </p>
-                  {productType.eachProduct.map((each, key) => (
+                  {productType.eachProduct.map((each, key) => !each ? (
+                    <>
+                      The products are not Available
+                    </>
+                  ) : (
                     <Block key={key}>
                       <h2>{each.title} </h2>
                       <Yoga maxCol={3}>
-                        {each.images.map((img, key) => (
+                        {!each.images ? null : each.images.map((img, key) => (
                           <div key={key}>
                             <img src={`${img.url}`} width={'100%'} />
                           </div>
