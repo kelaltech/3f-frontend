7547919b3a67b91d6e95a1601475f5c3bc1008f1
dagem <dagixmeko@gmail.com>
2020-12-02 15:00:06 +0300

    Google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,27 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 
+const { NEXT_GTM_ID } = process.env
+
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
+          {!NEXT_GTM_ID ? null : (
+            <>
+              <link
+                rel="dns-prefetch"
+                href="https://www.googletagmanager.com/"
+              />
+              <link rel="preconnect" href="https://www.googletagmanager.com/" />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.defer=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${NEXT_GTM_ID}');`,
+                }}
+              ></script>
+            </>
+          )}
+
           <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
@@ -38,6 +55,12 @@ class MyDocument extends Document {
         </Head>
 
         <body>
+          <noscript>
+            <iframe src={`https://www.googletagmanager.com/ns.html?id=${NEXT_GTM_ID}`}
+              height={0} width={0} style={{ display: 'none', visibility: 'hidden' }}>
+            </iframe>
+          </noscript>
+
           <Main />
           <NextScript />
         </body>
